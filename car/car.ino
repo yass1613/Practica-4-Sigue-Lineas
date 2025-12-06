f4a0a6b74c1de3c21365e31e0d3f81a2c99625a7
Mateo Ferreira <m.ferreira.2023@alumnos.urjc.es>
2025-12-06 20:51:02 +0100

    avance practica

--- car/car.ino
+++ car/car.ino
@@ -7,6 +7,8 @@
 #define PIN_ITR20001_LEFT   A2
 #define PIN_ITR20001_MIDDLE A1
 #define PIN_ITR20001_RIGHT  A0
+
+#define LINE_TRESHOLD 900
 // Enable/Disable motor control.
 //  HIGH: motor control enabled
 //  LOW: motor control disabled
@@ -27,60 +29,164 @@
 #define NUM_LEDS 1
 
 #define PERIODIC_T1 100
-#define COMPUTATION_TIME_ON_T1 270
+#define PERIODIC_T2 200
+#define PERIODIC_T3 4000
+#define PERIODIC_T4 150
 CRGB leds[NUM_LEDS];
 
+volatile long duration;
+volatile float distance;
+volatile int left_IR, right_IR, mid_IR;
+volatile int endLap = 0;
+bool lineLost = false;
+volatile int totalCounter = 0, lineCounter = 0;
+unsigned long startTime;
 
+void sendMsg(int action, unsigned long arg) {
+  Serial.print("{");
+  Serial.print(action);
 
-long duracion, distancia;
-int left_IR, right_IR, mid_IR;
+  if (arg) {
+    Serial.print(":");
+    Serial.print(arg);
+  }
 
-bool read_US() {
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
-  duracion = pulseIn(ECHO_PIN, HIGH, 30000);
-  distancia = duracion * VEL_SOUND / 2;
-
+  Serial.println("}");
 }
 
-static void Task1(void *parameters) {
-  TickType_t xLastWakeTime, aux;
+uint32_t Color(uint8_t r, uint8_t g, uint8_t b)
+{
+  return (((uint32_t)r << 16) | ((uint32_t)g << 8) | b);
+}
 
-  while(1) {
-    uint32_t start = micros();
+
+static void Task1(void *parameters) {
+  TickType_t xLastWakeTime;
+
+  while(!endLap) {
     xLastWakeTime = xTaskGetTickCount();
-    aux = xLastWakeTime;
 
     left_IR = analogRead(PIN_ITR20001_LEFT);
     mid_IR  = analogRead(PIN_ITR20001_MIDDLE);
     right_IR = analogRead(PIN_ITR20001_RIGHT);
-
-    while ((aux - xLastWakeTime) * portTICK_PERIOD_MS < COMPUTATION_TIME_ON_T1) {
-      aux = xTaskGetTickCount();
+    /*    
+    Serial.print("IR L:");
+    Serial.print(left_IR);
+    Serial.print(" M:");
+    Serial.print(mid_IR);
+    Serial.print(" R:");
+    Serial.println(right_IR);
+    */
+    if (left_IR < LINE_TRESHOLD && mid_IR < LINE_TRESHOLD && right_IR < LINE_TRESHOLD) {
+      if (!lineLost) {
+        lineLost = true;
+        sendMsg(3, 0);
+        sendMsg(5, 0);
+      }
+    } else {
+      lineCounter++;
+      if (lineLost){
+        lineLost = false;
+        sendMsg(7, 0);
+        sendMsg(6, 0);
+      }
     }
-
-    uint32_t cost = micros() - start;
-    Serial.print("Coste T1= ");
-    Serial.println(cost);
-
+    totalCounter++;
     xTaskDelayUntil(&xLastWakeTime, (PERIODIC_T1 / portTICK_PERIOD_MS));
   }
 }
 
+static void Task2(void *parameters) {
+  TickType_t xLastWakeTime;
+
+  while(!endLap) {
+    xLastWakeTime = xTaskGetTickCount();
+
+    if (left_IR > LINE_TRESHOLD || mid_IR > LINE_TRESHOLD || right_IR > LINE_TRESHOLD)
+      FastLED.showColor(Color(0, 255, 0));
+    else FastLED.showColor(Color(255, 0, 0));
+
+    xTaskDelayUntil(&xLastWakeTime, (PERIODIC_T2 / portTICK_PERIOD_MS));
+  }
+}
+
+static void Task3(void *parameters) {
+  TickType_t xLastWakeTime;
+
+  while(!endLap) {
+    xLastWakeTime = xTaskGetTickCount();
+    unsigned long time = millis() - startTime;
+    sendMsg(4, time);
+
+    xTaskDelayUntil(&xLastWakeTime, (PERIODIC_T3 / portTICK_PERIOD_MS));
+  }
+}
+
+static void Task4(void *parameters) {
+  TickType_t xLastWakeTime;
+
+  while(!endLap) {
+    xLastWakeTime = xTaskGetTickCount();
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH, 30000);
+    if (duration > 0) { 
+      distance = duration * VEL_SOUND / 2;
+      if (distance < 8.2) {
+        endLap = 1;
+        sendMsg(2, distance);
+        unsigned long time = millis() - startTime;
+        sendMsg(1, time);
+      }
+    }
+
+    xTaskDelayUntil(&xLastWakeTime, (PERIODIC_T4 / portTICK_PERIOD_MS));
+  }
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
   FastLED.setBrightness(20);
+  FastLED.showColor(Color(0, 0, 255));
 
   pinMode(PIN_ITR20001_LEFT,   INPUT);
   pinMode(PIN_ITR20001_MIDDLE, INPUT);
   pinMode(PIN_ITR20001_RIGHT,  INPUT);
 
-  xTaskCreate(Task1, "ReadItr", 128, NULL, 1, NULL);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 
+
+  String sendBuff;
+
+
+  while(1) {
+
+    if (Serial.available()) {
+
+      char c = Serial.read();
+      sendBuff += c;
+      
+      if (c == '}')  {
+        if (sendBuff == "{OK}") {
+          break; 
+        }          
+        sendBuff = "";
+      } 
+    }
+  }
+
+  xTaskCreate(Task1, "ReadItr", 100, NULL, 4, NULL);
+  xTaskCreate(Task2, "LED", 100, NULL, 1, NULL);
+  xTaskCreate(Task3, "Ping", 100, NULL, 2, NULL);
+  xTaskCreate(Task4, "UltraSound", 100, NULL, 3, NULL);
+
+  sendMsg(0, 0);
+  startTime = millis();
 
 }
 
--- esp/esp.ino
+++ esp/esp.ino
@@ -1,21 +1,119 @@
-#include <WiFi.h> //Wifi library
+#include <WiFi.h> 
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#include "secrets.h"
+#define RXD2 33
+#define TXD2 4
+#define MQTT_SERVER      "193.147.79.118"
+#define MQTT_SERVERPORT  21883
+#define MQTT_USERNAME    ""
+#define MQTT_PASSWORD    ""
+
+WiFiClient wifiClient;
+Adafruit_MQTT_Client mqtt(&wifiClient, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
+String topicStr = "/SETR/2025/4/";
+Adafruit_MQTT_Publish pub_robot = Adafruit_MQTT_Publish(&mqtt, topicStr.c_str());
+
+void MQTT_connect() {
+  int8_t ret;
+
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Retrying in 5 seconds...");
+    mqtt.disconnect();
+    delay(5000);
+  }
+
+  Serial.println("MQTT CONNECTED!");
+}
+
+void pubMsg(int action, unsigned long arg) {
+  String json;
+
+  switch(action) {
+    
+    case 0: // START_LAP
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"START_LAP\"}";
+      break;
+
+    case 1: // END_LAP
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"END_LAP\",\"time\":" 
+        + String(arg) + "}";
+      break;
+
+    case 2: // OBSTACLE_DETECTED
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"OBSTACLE_DETECTED\",\"distance\":" 
+        + String(arg) + "}";
+      break;
+
+    case 3: // LINE_LOST
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"LINE_LOST\"}";
+      break;
+
+    case 4: // PING
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"PING\",\"time\":" 
+        + String(arg) + "}";
+      break;
+
+    case 5: // INIT_LINE_SEARCH
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"INIT_LINE_SEARCH\"}";
+      break;
+
+    case 6: // STOP_LINE_SEARCH
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"STOP_LINE_SEARCH\"}";
+      break;
+
+    case 7: // LINE_FOUND
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"LINE_FOUND\"}";
+      break;
+
+    case 8: // VISIBLE_LINE
+      json = "{\"team_name\":\"Maradona\",\"id\":\"4\",\"action\":\"VISIBLE_LINE\",\"value\":" 
+        + String(arg) + "}";
+      break;
+
+    default:
+      Serial.println("Error receiving from Arduino");
+      break;
+  }
+  pub_robot.publish(json.c_str());
+}
 
 
-// Recommendation. The following information is sensitive. Better
-// if you save those variables in a different header file and make sure that
-// new file is not pushed at any github public repository
-#define EAP_ANONYMOUS_IDENTITY "20220719anonymous@urjc.es" // leave as it is
-#define EAP_IDENTITY "USER@urjc.es"    // Use your URJC email
-#define EAP_PASSWORD "PASSWORD"            // User your URJC password
-#define EAP_USERNAME "USER@urjc.es"    // Use your URJC email
+void processMsg(String msg) {
+  msg.replace("{", "");
+  msg.replace("}", "");
 
+  int action = -1, sep = msg.indexOf(':');
+  unsigned long arg = 0;
+
+  if (sep == -1) {
+    action = msg.toInt();
+  } else {
+    action = msg.substring(0, sep).toInt();
+    arg = msg.substring(sep + 1).toInt();
+  }
+
+  pubMsg(action, arg);
+}
+
+/*
 //SSID NAME
 const char* ssid = "eduroam"; // eduroam SSID
 
 
 void setup() {
   Serial.begin(115200);
-  delay(10);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+
+  delay(5000);
+
   Serial.print(F("Connecting to network: "));
   Serial.println(ssid);
   WiFi.disconnect(true); 
@@ -30,9 +128,63 @@ void setup() {
   Serial.println(F("WiFi is connected!"));
   Serial.println(F("IP address set: "));
   Serial.println(WiFi.localIP()); //print LAN IP
+
+  MQTT_connect();
+
+  Serial2.print("{OK}");
+  Serial.print("Messase sent! to Arduino");
 }
 
 void loop() {
   yield();
 }
+*/
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+
+  delay(5000);
+
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nConnected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  MQTT_connect();
+
+  Serial2.print("{OK}");
+  Serial.print("Messase sent! to Arduino");
+}
+
+String sendBuff;
+
+void loop() {
+    if (Serial2.available()) {
+
+      char c = Serial2.read();
+      sendBuff += c;
+      
+      if (c == '}')  {            
+        Serial.print("Received data in serial port from Arduino: ");
+        Serial.println(sendBuff);
+
+        processMsg(sendBuff);
+        sendBuff = "";
+      } 
+  }
+
+}
+
+
+
 
